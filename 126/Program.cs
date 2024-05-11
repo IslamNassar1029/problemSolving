8908aa52d77f19896981b17dac5e6e7d48b02bc0
Islam Nassar <islamnassar10297@gmail.com>
2024-05-11 20:07:49 +0300

    Problem 126

---
+++ 126/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _126
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int noOfForces = int.Parse(Console.ReadLine());
+            int[] SumofForces = {0,0,0};    
+            for (int i = 0; i < noOfForces; i++)
+            {
+                string[] forces = Console.ReadLine().Split(' ');
+                SumofForces[0] += int.Parse(forces[0]);
+                SumofForces[1] += int.Parse(forces[1]);
+                SumofForces[2] += int.Parse(forces[2]);
+            }
+            var isItInEquilibrium = SumofForces.FirstOrDefault(x => x != 0);
+            Console.WriteLine(isItInEquilibrium == 0 ? "YES" : "NO");
+        }
+    }
+}
