04034913453ff3720dbd4a37384aa09922be7c0d
Islam Nassar <islamnassar10297@gmail.com>
2024-05-12 19:23:07 +0300

    127

---
+++ 127/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _127
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var noOfOperations=Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+
+            for (int i = 0; i < noOfOperations; i++)
+            {
+                var operation = Console.ReadLine();
+                if (operation[1] is '+')
+                {
+                    result++;
+                }
+                else
+                {
+                    result--;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
