d5f12f836ef7991c8fc3d6d2ae28a062a96b81a7
Islam Nassar <islamnassar10297@gmail.com>
2024-05-13 11:40:52 +0300

    128

---
+++ 128/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _128
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lengthOfString = Convert.ToInt32(Console.ReadLine());
+            var stringofChar = Console.ReadLine();
+            int noOf0=0, noOf1=0;   
+            for (int i = 0; i < lengthOfString; i++)
+            {
+                if (stringofChar[i]=='1')
+                {
+                    noOf1++;
+                }
+                else
+                {
+                    noOf0++;
+                }
+            }
+            Console.WriteLine(Math.Abs( noOf0-noOf1));
+
+        }
+    }
+}
