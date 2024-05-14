40e7dce26a541023dadb1f7b53b8d1e7da0fec36
Islam Nassar <islamnassar10297@gmail.com>
2024-05-14 08:52:52 +0300

    129

---
+++ 129/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _129
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstWord = Console.ReadLine();
+            var secondWord = Console.ReadLine();
+            if (firstWord is null)
+            {
+                return;
+            }
+            char[] charArray = firstWord.ToCharArray();
+            Array.Reverse(charArray);
+            string reversedFirstWord = new string(charArray);
+
+            Console.WriteLine(reversedFirstWord == secondWord ? "YES" : "NO");
+        }
+    }
+}
