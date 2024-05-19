7ddc1d9c5e2bee45f02d1396f7be9960522ebb7a
Islam Nassar <islamnassar10297@gmail.com>
2024-05-19 18:24:15 +0300

    131

---
+++ 131/Program.cs
@@ -0,0 +1,79 @@
+﻿//using System.Collections.Generic;
+
+//namespace _131
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            var noOfLabTop= Convert.ToInt32(Console.ReadLine());
+//            Dictionary<int,int> laptops = new Dictionary<int,int>();
+//            for (int i = 0; i < noOfLabTop; i++)
+//            {
+//                var laptop = Console.ReadLine().Split(" ");
+//                int price = Convert.ToInt32(laptop[0]);
+//                int quality = Convert.ToInt32(laptop[1]);
+//                laptops[price] = quality;
+//            }
+//            bool AlexIsRight=false;
+//            foreach (var labtopfirst in laptops)
+//            {
+//                foreach (var otherLabtop in laptops)
+//                {
+//                    if (labtopfirst.Key<otherLabtop.Key&&labtopfirst.Value>otherLabtop.Value)
+//                    {
+//                        AlexIsRight = true;
+//                        break;
+//                    }
+//                    else if(labtopfirst.Key > otherLabtop.Key && labtopfirst.Value < otherLabtop.Value)
+//                    {
+//                        AlexIsRight = true;
+//                        break;
+//                    }
+//                }
+//                if( AlexIsRight)break;
+//            }
+
+//            Console.WriteLine(AlexIsRight? "Happy Alex":"Poor Alex");
+
+//        }
+//    }
+//}
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _131
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int noOfLaptops = Convert.ToInt32(Console.ReadLine());
+            List<(int price, int quality)> laptops = new List<(int, int)>();
+
+            for (int i = 0; i < noOfLaptops; i++)
+            {
+                var laptop = Console.ReadLine().Split(" ");
+                int price = Convert.ToInt32(laptop[0]);
+                int quality = Convert.ToInt32(laptop[1]);
+                laptops.Add((price, quality));
+            }
+            laptops.Sort((x, y) => x.price.CompareTo(y.price));
+
+            bool AlexIsRight = false;
+            for (int i = 0; i < laptops.Count - 1; i++)
+            {
+                if (laptops[i].quality > laptops[i + 1].quality)
+                {
+                    AlexIsRight = true;
+                    break;
+                }
+            }
+
+            Console.WriteLine(AlexIsRight ? "Happy Alex" : "Poor Alex");
+        }
+    }
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var word = Console.ReadLine();
+            string result = "";
+
+            foreach (var item in word)
+            {
+                char lowerCaseItem = char.ToLower(item); 
+                if ("aoyeui".Contains(lowerCaseItem)) 
+                {
+                    continue; 
+                }
+                else
+                {
+                    result += "." + lowerCaseItem; 
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
