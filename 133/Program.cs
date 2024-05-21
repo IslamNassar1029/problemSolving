35f0214749c54182948d634d8cb7aca1f53faac5
Islam Nassar <islamnassar10297@gmail.com>
2024-05-21 09:40:23 +0300

    133

---
+++ 133/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _133
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int noOfRooms = Convert.ToInt32(Console.ReadLine());
+
+            int sumOfRooms = 0;
+
+            for (int i = 0; i < noOfRooms; i++)
+            {
+                string[] roomDiscription = Console.ReadLine().Split();
+                int noOfPeople = int.Parse(roomDiscription[0]);
+                int roomCapacity = int.Parse(roomDiscription[1]);
+                if (roomCapacity-noOfPeople >=2)
+                {
+                    sumOfRooms++;
+                }
+            }
+            Console.WriteLine(sumOfRooms);
+        }
+    }
+}
