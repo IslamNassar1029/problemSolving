bf75b177ac428a024632eaae473dec35d52a5b93
Islam Nassar <islamnassar10297@gmail.com>
2024-05-24 15:43:47 +0300

    136

---
+++ 136/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string input = Console.ReadLine();
+int[] positions = input.Split(' ').Select(int.Parse).ToArray();
+
+Array.Sort(positions);
+
+int totalDistance = Math.Abs(positions[0] - positions[2]);
+
+Console.WriteLine(totalDistance);
