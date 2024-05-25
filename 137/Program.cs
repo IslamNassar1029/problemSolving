20316024df6d64431228fdd198dd51e6c0a05165
Islam Nassar <islamnassar10297@gmail.com>
2024-05-25 10:10:04 +0300

    137

---
+++ 137/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _137
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int distance = Convert.ToInt32(Console.ReadLine());
+            int steps = 0;
+
+            while (distance > 0)
+            {
+                if (distance >= 5)
+                {
+                    getSteps(ref distance, 5, ref steps);
+                }
+                else if (distance >= 4)
+                {
+                    getSteps(ref distance, 4, ref steps);
+                }
+                else if (distance >= 3)
+                {
+                    getSteps(ref distance, 3, ref steps);
+                }
+                else if (distance >= 2)
+                {
+                    getSteps(ref distance, 2, ref steps);
+                }
+                else if (distance >= 1)
+                {
+                    getSteps(ref distance, 1, ref steps);
+                }
+            }
+
+            Console.WriteLine(steps);
+        }
+
+        static void getSteps(ref int distance, int stepLength, ref int steps)
+        {
+            steps += distance / stepLength;
+            distance %= stepLength;
+        }
+    }
+}
